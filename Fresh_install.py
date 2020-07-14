5a370f752282a620c11c2767ae4e4c99673b6a98
Vikas Goel <vikas.aero@Gmail.com>
2020-07-14 17:02:31 +0530

    Add files via upload

---
+++ Fresh_install.py
@@ -0,0 +1,15 @@
+from os import system
+
+system(f"python -m pip install --upgrade pip")
+system(f"pip install --upgrade wheel cmake")
+system(f"pip install --upgrade numpy pandas matplotlib seaborn xlrd joblib plotly scikit-learn lightgbm xgboost")
+system(f"pip install --upgrade imutils flask flask_Cors")
+system(f"pip install --upgrade opencv_python PyInstaller boto3 fast_enum pytesseract json2table beautifulsoup4")
+system(f"pip install --upgrade google-cloud google-cloud-vision google-cloud-storage google-api-core")
+system(f"pip install --upgrade pdf2image pyPDF4 pdfkit imblearn ply pyspellchecker passporteye")
+system(f"pip install --upgrade lxml gensim pandas-profiling pipreqs vulture")
+system(f"pip install --upgrade spacy")
+system(f"pip install --upgrade tensorflow")
+system(f"pip install --upgrade face_recognition datapackage wordcloud pydotplus unidecode pgeocode geotext")
+system(f"python -m spacy download en_core_web_md")
+system(f"python update_from_freeze_file.py")
