847c2405cc68e9880f41344fbc8d6a43089abbdb
Vikas Goel <vikas.aero@Gmail.com>
2023-07-20 15:06:30 +0530

    Update fresh_install_from_freeze_txt_file.py

--- fresh_install_from_freeze_txt_file.py
+++ fresh_install_from_freeze_txt_file.py
@@ -9,6 +9,6 @@ with open(freeze_file, 'r') as f:
         if "==" in line:
             library = line.split("==")[0]
             print(f"\ninstalling Library : {library}\n")
-            os.system(f"pip install --upgrade {library}")
+            os.system(f"pip install --no-deps --upgrade {library}")
 
 os.system(f"python -m spacy download en_core_web_md")
